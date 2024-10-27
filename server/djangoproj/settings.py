949fa8cfe33074ba31bd697312d4ab688448c46c
vidisha agarwal <agarwalvidisha39@gmail.com>
2024-10-27 01:45:28 -0400

    ci cd issue fix

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,8 +27,10 @@ SECRET_KEY =\
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', 'https://agarwalvidis-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://agarwalvidis-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 'https://agarwalvidis-8000.' + 
+'theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://agarwalvidis-8000.theiadockernext-0-labs-prod-theiak8s-4-' + 
+'tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -93,7 +95,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.' + 
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
